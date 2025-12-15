1693f7609af08a16152cbdf6da05e19ff006608a
Crim <unalof@gmail.com>
2025-12-15 23:39:50 +0300

    small fix

--- main.py
+++ main.py
@@ -220,7 +220,7 @@ class PhotoshopApp(QMainWindow):
         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
-        qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.RGB888)
+        qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
         pixmap = QPixmap.fromImage(qt_image)
 
         if hasattr(self, 'lbl_video'):
